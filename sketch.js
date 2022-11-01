91fe417532c00468a0d4b3f685ecc1e1610eabdf
Pennone <fabio.pennone@mail.polimi.it>
2022-11-01 04:08:22 +0100

    completed

--- sketch.js
+++ sketch.js
@@ -10,10 +10,16 @@ let BL_Image;
 let MS_Image;
 let VD_Image;
 
+let Lips;
+let Sing;
+
 let Waiting;
 
 let Static = [];
 let Static_Sound;
+
+let message;
+let startup = false;
 
 function preload() {
 	Static = loadImage("./assets/static.gif");
@@ -23,6 +29,19 @@ function preload() {
 	BL_Image = loadImage("./assets/Blind_Lemon.png");
 	MS_Image = loadImage("./assets/Mamie_Smith.png");
 	VD_Image = loadImage("./assets/Vernon_Dalhart.png");
+
+	Lips = loadImage("./assets/Lips.gif");
+	Sing = loadImage("./assets/Sing.gif");
+
+	AJ_Song = loadSound("./assets/Al_Jolson.ogg");
+	BS_Song = loadSound("./assets/Bessie_Smith.ogg");
+	BL_Song = loadSound("./assets/Blind_Lemon.ogg");
+	MS_Song = loadSound("./assets/Mamie_Smith.ogg");
+	VD_Song = loadSound("./assets/Vernon_Dalhart.ogg");
+
+	Static_Sound = loadSound("./assets/Static.ogg");
+	Waiting = loadSound("./assets/Waiting.ogg");
+
 
 }
 
@@ -35,6 +54,11 @@ function setup() {
   	textSize(Text_Size);
 
 	rectMode(CENTER);
+	ellipseMode(CENTER);
+
+	noCursor();
+
+	message = "Click to start";
 
 }
 
@@ -54,23 +78,93 @@ function draw() {
 
 	image(VD_Image, width - (VD_Image.width * Image_Scale*1.7), -75, VD_Image.width * (Image_Scale*1.7), VD_Image.height * (Image_Scale*1.7));
 
-	image(AJ_Image, width - (AJ_Image.width * Image_Scale*0.8), height - (AJ_Image.height * Image_Scale*0.8), AJ_Image.width * (Image_Scale*0.8), AJ_Image.height * (Image_Scale*0.8));
-
 	push();
+
 	noStroke();
 	fill(255, 255, 255, 191);
-	rect(width/2, height/2, width/3, height/2, 20);
-
-	if (windowWidth < 775){
-		image(BS_Image, width/2 - BS_Image.width * Image_Scale/8 * (Image_Scale*1.2), height - BS_Image.height * (Image_Scale*1.2), BS_Image.width * (Image_Scale*1.2), BS_Image.height * (Image_Scale*1.2));
-	} else {
-		image(BS_Image, width/2 - BS_Image.width * Image_Scale/8 * (Image_Scale*1.2), height - BS_Image.height * (Image_Scale*1.2), BS_Image.width * (Image_Scale*1.2), BS_Image.height * (Image_Scale*1.2));
-	}
-	pop();
 	
-	const message1 = "(WIP)\nRoyalty Free\n 20s Tunes\n\nAl Jolson\nBessie Smith\nBlind Lemon Jefferson\nMamie Smith\nVernon Dalhart";
-	text(message1, width/2, height/2);
+	rect(width/2, height/4, width/3, height/2, 20);
 
+	image(AJ_Image, width - (AJ_Image.width * Image_Scale*0.8), height - (AJ_Image.height * Image_Scale*0.8), AJ_Image.width * (Image_Scale*0.8), AJ_Image.height * (Image_Scale*0.8));
+	
+	fill("black");
+	text(message, width/2, height/4);
 
+	image(BS_Image, width/2 - BS_Image.width * Image_Scale/1.2 * (Image_Scale*1.3), height - BS_Image.height * (Image_Scale*1.1), BS_Image.width * (Image_Scale*1.3), BS_Image.height * (Image_Scale*1.3));
 
+	//prendere le misure
+	fill(255, 255, 255, 100);
+	//circle(MS_Image.width / 1.35 * (Image_Scale*2.5), MS_Image.height / 3 * (Image_Scale*2.5), 100);
+	//circle(BL_Image.width / 2.5 * (Image_Scale*2), height - (BL_Image.height * Image_Scale) + BL_Image.height / 3.9 * (Image_Scale*2), 100);
+	//circle(width - (VD_Image.width * Image_Scale*1.7) + VD_Image.width / 2 * (Image_Scale*1.7), -75 + VD_Image.height / 3.5 * (Image_Scale*1.7), 100);
+	//circle(width/2 - BS_Image.width * Image_Scale/1.2 * (Image_Scale*1.3) + BS_Image.width / 1.75 * (Image_Scale*1.3), height - BS_Image.height * (Image_Scale*1.1) + BS_Image.height / 2 * (Image_Scale*1.3), 100);
+	//circle(width - (AJ_Image.width * Image_Scale*0.8) + AJ_Image.width / 2 * (Image_Scale*0.8), height - (AJ_Image.height * Image_Scale*0.8) + AJ_Image.height / 1.6 * (Image_Scale*0.8), 100);
+	
+	pop();
+
+	//rect(mouseX, mouseY, 100);
+	if(startup == true) {
+		if(mouseX >= MS_Image.width / 1.35 * (Image_Scale*2.5) - 50 && mouseX <= MS_Image.width / 1.35 * (Image_Scale*2.5) + 50 && mouseY >= MS_Image.height / 3 * (Image_Scale*2.5) - 50 && mouseY <= MS_Image.height / 3 * (Image_Scale*2.5) + 50){
+			image(Sing, mouseX - 100 * Image_Scale, mouseY - 100 * Image_Scale, 200 * Image_Scale, 200 * Image_Scale);
+			MS_Song.setVolume(0.7);
+			Static_Sound.setVolume(0.1);
+		} else if(mouseX >= BL_Image.width / 2.5 * (Image_Scale*2) - 50 && mouseX <= BL_Image.width / 2.5 * (Image_Scale*2) + 50 && mouseY >= height - (BL_Image.height * Image_Scale) + BL_Image.height / 3.9 * (Image_Scale*2) - 50 && mouseY <= height - (BL_Image.height * Image_Scale) + BL_Image.height / 3.9 * (Image_Scale*2) + 50) {
+			image(Sing, mouseX - 100 * Image_Scale, mouseY - 100 * Image_Scale, 200 * Image_Scale, 200 * Image_Scale);
+			BL_Song.setVolume(0.7);
+			Static_Sound.setVolume(0.1);
+		} else if (mouseX >= width - (VD_Image.width * Image_Scale*1.7) + VD_Image.width / 2 * (Image_Scale*1.7) - 50 && mouseX <= width - (VD_Image.width * Image_Scale*1.7) + VD_Image.width / 2 * (Image_Scale*1.7) + 50 && mouseY >= -75 + VD_Image.height / 3.5 * (Image_Scale*1.7) - 50 && mouseY <= -75 + VD_Image.height / 3.5 * (Image_Scale*1.7) + 50) {
+			image(Sing, mouseX - 100 * Image_Scale, mouseY - 100 * Image_Scale, 200 * Image_Scale, 200 * Image_Scale);
+			VD_Song.setVolume(0.7);
+			Static_Sound.setVolume(0.1);
+		} else if (mouseX >= width/2 - BS_Image.width * Image_Scale/1.2 * (Image_Scale*1.3) + BS_Image.width / 1.75 * (Image_Scale*1.3) - 50 && mouseX <= width/2 - BS_Image.width * Image_Scale/1.2 * (Image_Scale*1.3) + BS_Image.width / 1.75 * (Image_Scale*1.3) + 50 && mouseY >= height - BS_Image.height * (Image_Scale*1.1) + BS_Image.height / 2 * (Image_Scale*1.3) - 50 && mouseY <= height - BS_Image.height * (Image_Scale*1.1) + BS_Image.height / 2 * (Image_Scale*1.3) + 50) {
+			image(Sing, mouseX - 100 * Image_Scale, mouseY - 100 * Image_Scale, 200 * Image_Scale, 200 * Image_Scale);
+			BS_Song.setVolume(0.7);
+			Static_Sound.setVolume(0.1);
+		} else if (mouseX >= width - (AJ_Image.width * Image_Scale*0.8) + AJ_Image.width / 2 * (Image_Scale*0.8) - 50 && mouseX <= width - (AJ_Image.width * Image_Scale*0.8) + AJ_Image.width / 2 * (Image_Scale*0.8) + 50 && mouseY >= height - (AJ_Image.height * Image_Scale*0.8) + AJ_Image.height / 1.6 * (Image_Scale*0.8) - 50 && mouseY <= height - (AJ_Image.height * Image_Scale*0.8) + AJ_Image.height / 1.6 * (Image_Scale*0.8) + 50) {
+			image(Sing, mouseX - 100 * Image_Scale, mouseY - 100 * Image_Scale, 200 * Image_Scale, 200 * Image_Scale);
+			AJ_Song.setVolume(0.7);
+			Static_Sound.setVolume(0.1);
+		} else if (mouseX >= width/3 && mouseX <= width - width/3 && mouseY <= height/2) {
+			image(Lips, mouseX - 100 * Image_Scale, mouseY - 100 * Image_Scale, 200 * Image_Scale, 200 * Image_Scale);
+			AJ_Song.setVolume(0.7);
+			Static_Sound.setVolume(0.3);
+			AJ_Song.setVolume(0);
+			BS_Song.setVolume(0);
+			BL_Song.setVolume(0);
+			MS_Song.setVolume(0);
+			VD_Song.setVolume(0);
+			Waiting.setVolume(0.7);
+		} else {
+			image(Lips, mouseX - 100 * Image_Scale, mouseY - 100 * Image_Scale, 200 * Image_Scale, 200 * Image_Scale);
+			AJ_Song.setVolume(0);
+			BS_Song.setVolume(0);
+			BL_Song.setVolume(0);
+			MS_Song.setVolume(0);
+			VD_Song.setVolume(0);
+			Static_Sound.setVolume(0.8);
+			Waiting.setVolume(0);
+		}
+	}
+
+}
+
+function mouseClicked() {
+	if(startup == false) {
+	startup = true;
+	message = ("Royalty Free\n 20s Tunes\n\nMamie Smith\nBlind Lemon Jefferson\nBessie Smith\nAl Jolson\nVernon Dalhart");
+	Static_Sound.setVolume(0.8);
+	Static_Sound.loop();
+	AJ_Song.setVolume(0);
+	BS_Song.setVolume(0);
+	BL_Song.setVolume(0);
+	MS_Song.setVolume(0);
+	VD_Song.setVolume(0);
+	AJ_Song.loop();
+	BS_Song.loop();
+	BL_Song.loop();
+	MS_Song.loop();
+	VD_Song.loop();
+	Waiting.setVolume(0);
+	Waiting.loop();
+	}
 }
